15d49791f3b63353eac0f97b6c86c9b2b2eb69bf
Vbahir7 <bahir.vaibhav@gmail.com>
2024-10-13 18:14:34 +0530

    test2

---
+++ src/Task/Lab005.java
@@ -0,0 +1,10 @@
+package Task;
+
+//print even number using for loop
+
+public class Lab005 {
+    public static void main(String[] args) {
+        for (int i=1;i<=25;i++)
+        System.out.println(2*i);
+    }
+}
---
+++ src/Task/Lab006.java
@@ -0,0 +1,18 @@
+package Task;
+
+//even number using for loop2
+//odd number using for loop
+
+public class Lab006 {
+    public static void main(String[] args) {
+        System.out.println("Even Numbers");
+        for (int i=2;i<=50;i++) {
+             System.out.println(i++);
+        }
+        System.out.println("------------------------");
+        System.out.println("Odd Numbers");
+        for (int i=1;i<=50;i++) {
+            System.out.println(i++);
+        }
+    }
+}
